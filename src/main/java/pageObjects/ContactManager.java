2883101a2ec4ad89838bde50361d6e7494bcb106
Veronika_Filicheva <veronika_filicheva@epam.com>
2019-03-25 15:12:57 +0300

    hw2

---
+++ src/main/java/pageObjects/ContactManager.java
@@ -0,0 +1,49 @@
+package pageObjects;
+
+import io.appium.java_client.HasOnScreenKeyboard;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class ContactManager {
+
+    private final String appPackageName = "com.example.android.contactmanager:id/";
+    private AndroidDriver driver;
+    private By contactTitle = By.id("android:id/title");
+    private By addContactButton = By.id(appPackageName + "addContactButton");
+    private By targetAccountSpinner = By.id(appPackageName + "accountSpinner");
+    private By contactNameField = By.id(appPackageName + "contactNameEditText");
+    private By contactFormEditField = By.id(appPackageName + "contactPhoneEditText");
+    private By contactEmailField = By.id(appPackageName + "contactEmailEditText");
+
+    public ContactManager (AndroidDriver driver) {this.driver = driver;}
+
+
+    public void clickOnAddButton() {
+        driver.findElement(addContactButton).click();
+    }
+
+    public void checkTitle() {
+        assertTrue(driver.findElement(contactTitle).isDisplayed());
+    }
+
+    public void checkElementsDisplayed() {
+        assertTrue(driver.findElement(targetAccountSpinner).isDisplayed());
+        assertTrue(driver.findElement(contactNameField).isDisplayed());
+        assertTrue(driver.findElement(contactFormEditField).isDisplayed());
+        assertTrue(driver.findElement(contactEmailField).isDisplayed());
+
+    }
+
+    public void checkVirtualKeybordIsShown() {
+        assertTrue(((HasOnScreenKeyboard) driver).isKeyboardShown());
+    }
+
+
+}
--- src/test/java/scenarios/nativeTests/ContactManagerTest.java
+++ src/test/java/scenarios/nativeTests/ContactManagerTest.java
@@ -1,72 +1,32 @@
 package scenarios.nativeTests;
 
-import io.appium.java_client.HasOnScreenKeyboard;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import pageObjects.ContactManager;
 import scenarios.Hooks;
 import setup.Properties;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Test(groups = "native")
 public class ContactManagerTest extends Hooks {
     // Send PropertyFile in the Hooks to read from correct property file in  prepareDriver() in @BeforeSuite
-    protected ContactManagerTest() {
+    protected ContactManagerTest() throws Exception {
         super(Properties.NATIVE);
     }
 
     @Test(description = "Open Add Contact screen and check elements displaying")
     public void simplestAddContactScreenTest() throws Exception {
+        ContactManager contactManager = new ContactManager(driver());
 
-        String appPackageName = "com.example.android.contactmanager:id/";
-
-        // click on Add Contact Button
-        By addButton = By.id(appPackageName + "addContactButton");
-        driver().findElement(addButton).click();
+        // Click on Add Contact Button
+        contactManager.clickOnAddButton();
 
         //Check Add Contact Title
-        By addContactTitle = By.id("android:id/title");
-        Assert.assertTrue(driver().findElement(addContactTitle).isDisplayed());
+        contactManager.checkTitle();
 
-        //Find WebElements for each title
-        WebElement targetAccount = driver().findElement(By.id("Target Account"));
-        WebElement contactName = driver().findElement(By.id("Contact Name"));
-        WebElement contactPhone = driver().findElement(By.id("Contact Phone"));
-        WebElement contactEmail = driver().findElement(By.id("Contact Email"));
-
-        //Find List of WebElements for fields titles
-        List<WebElement> actualTitles = Arrays.asList(targetAccount, contactName,
-                contactPhone, contactEmail);
-        List<String> expectedTitles = Arrays.asList("Target Account", "Contact Name",
-                "Contact Phone", "Contact Email");
-
-        //Check that each title is displayed
-        for (WebElement title : actualTitles) {
-            Assert.assertTrue(title.isDisplayed());
-        }
-
-        //Check titles' name
-        for (int i = 0; i < expectedTitles.size(); i++) {
-            Assert.assertEquals(actualTitles.get(i).getText(), expectedTitles.get(i));
-        }
-
-        //Find all necessary fields on "Add Contact" screen
-        WebElement targetAccountSpinner = driver().findElement(By.id(appPackageName + "accountSpinner"));
-        WebElement contactNameField = driver().findElement(By.id(appPackageName + "contactNameEditText"));
-        WebElement contactPhoneField = driver().findElement(By.id(appPackageName + "contactPhoneEditText"));
-        WebElement contactEmailField = driver().findElement(By.id(appPackageName + "contactEmailEditText"));
-
-        //Assert all found fields are displayed
-        Assert.assertTrue(targetAccountSpinner.isDisplayed());
-        Assert.assertTrue(contactNameField.isDisplayed());
-        Assert.assertTrue(contactPhoneField.isDisplayed());
-        Assert.assertTrue(contactEmailField.isDisplayed());
+        //Check Elements Target Account,Contact Name, Contact Phone, Contact Email  Are Displayed
+        contactManager.checkElementsDisplayed();
 
         //Check virtual keyboard appears
-        Assert.assertTrue(((HasOnScreenKeyboard) driver()).isKeyboardShown());
+        contactManager.checkVirtualKeybordIsShown();
 
     }
 
